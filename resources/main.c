a085c2713c7b045176cb13b59f5cda6e0f529d96
Mateusz Woś <wos.mateusz16@gmail.com>
2017-12-01 14:45:46 +0100

    monte carlo src

--- resources/main.c
+++ resources/main.c
@@ -29,14 +29,14 @@ main (void)
     const gsl_rng_type *T;
     gsl_rng *r;
     gsl_monte_function G = { &g, 2, 0 };
-    FILE *p,*m,*v;
-    p= fopen("p.txt", "w");
-    m= fopen("m.txt", "w");
-    v= fopen("v.txt", "w");
+    FILE *plain,*monte,*vegas;
+    plain = fopen("plain.txt", "w");
+    monte = fopen("monte.txt", "w");
+    vegas = fopen("vegas.txt", "w");
 
 
     for(int i=100; i<10000;i++) {
-        size_t calls = i; // liczba wywolan funkcji
+        size_t calls = i;
         gsl_rng_env_setup();
         T = gsl_rng_default;
         r = gsl_rng_alloc(T);
@@ -44,24 +44,21 @@ main (void)
             gsl_monte_plain_state *s = gsl_monte_plain_alloc(2);
             gsl_monte_plain_integrate(&G, xl, xu, 2, calls, r, s, &res, &err);
             gsl_monte_plain_free(s);
-            fprintf(p, "%d\t\t\t", calls);
-            fprintf(p, "%f \n", err);
-          //  display_results("plain", res, err);
+            fprintf(plain, "%d\t\t\t", calls);
+            fprintf(plain, "%f \n", err);
         }
         {
             gsl_monte_miser_state *s = gsl_monte_miser_alloc(2);
             gsl_monte_miser_integrate(&G, xl, xu, 2, calls, r, s, &res, &err);
             gsl_monte_miser_free(s);
-            fprintf(m, "%d\t\t", calls);
-            fprintf(m, "%f \n", err);
-          //  display_results("miser", res, err);
+            fprintf(monte, "%d\t\t", calls);
+            fprintf(monte, "%f \n", err);
         }
-        { gsl_monte_vegas_state *s = gsl_monte_vegas_alloc(2);
-          gsl_monte_vegas_integrate(&G, xl, xu, 2, calls , r, s,&res, &err);
-           display_results("vegas final", res, err);
-            fprintf(v, "%d\t\t", calls);
-            fprintf(v, "%f \n", err);
-            gsl_monte_vegas_free(s);
+        { 	gsl_monte_vegas_state *s = gsl_monte_vegas_alloc(2);
+			gsl_monte_vegas_integrate(&G, xl, xu, 2, calls , r, s,&res, &err);
+			fprintf(vegas, "%d\t\t", calls);
+			fprintf(vegas, "%f \n", err);
+			gsl_monte_vegas_free(s);
         }
     }
     gsl_rng_free (r);
